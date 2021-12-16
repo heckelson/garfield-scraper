541d213091b624d50661d32d033b350d29fc68e3
heckelson <26191361+heckelson@users.noreply.github.com>
2021-12-16 17:52:48 +0100

    initial commit (works)

---
+++ main.py
@@ -0,0 +1,89 @@
+import shutil
+
+from bs4 import BeautifulSoup
+from urllib import request
+from pathlib import Path
+from shutil import rmtree
+
+MONTHS = ["January", "February", "March", "April", "May", "June",
+          "July", "August", "September", "October", "November", "December"]
+BANNED_IMAGES = ["/img/valid-xhtml10.gif", "/img/valid-css.gif", "/img/vim.gif"]
+BASE_URL = "http://pt.jikos.cz"
+GARFIELD_URL = "http://pt.jikos.cz/garfield/"
+BASE_DIR = "./garfield"
+
+
+def read_url(url: str):
+    file = request.urlretrieve(url)
+
+    text = ""
+    with open(file[0], 'r') as contents:
+        for line in contents.readlines():
+            text += line
+    return text
+
+
+def get_soup(file_contents):
+    return BeautifulSoup(file_contents, features='html.parser')
+
+
+def find_images_on_page(page: BeautifulSoup):
+    images = page.find_all('img')
+    images = list(filter(lambda x: str(x['src']).endswith("gif") and x['src'] not in BANNED_IMAGES, images))
+    return images
+
+
+def parse_date(_datestr: str):
+    spl = _datestr.split('/')
+    return spl[0], spl[1], spl[2]
+
+
+def download_image(imageurl, target_name, target_dir):
+    image_file, _ = request.urlretrieve(imageurl)
+
+    full_file_path = f"{target_dir}/{target_name}"
+    print(f"Downloading file {full_file_path}...")
+
+    shutil.move(image_file, full_file_path)
+
+
+def download_all_images_in(collection):
+    for image in collection:
+        image_url = image['src']
+        alt_text = image['alt']
+        _, datestring = alt_text.split(' ')
+        day, month, year = parse_date(datestring)
+
+        Path.mkdir(Path(f"{BASE_DIR}/{year}/{month}"), exist_ok=True)
+        download_image(image_url, f"{day}.gif", f"{BASE_DIR}/{year}/{month}")
+
+
+if __name__ == '__main__':
+    base_soup = get_soup(read_url(GARFIELD_URL))
+    year_links = base_soup.find_all('a')
+    year_links = list(filter(lambda lin: "garfield/" in str(lin), year_links))
+
+    print(f"Found {len(year_links)} links")
+
+    rmtree(BASE_DIR)
+    Path.mkdir(Path(BASE_DIR), exist_ok=True)
+
+    for year_link in year_links:
+        year = year_link.string
+        Path.mkdir(Path(f"{BASE_DIR}/{year}"), exist_ok=True)
+
+        first_month_page = get_soup(read_url(BASE_URL + year_link['href']))
+
+        # the images now have have the following format:
+        #   <img alt="garfield 19/6/1978" src="http://images.ucomics.com/comics/ga/1978/ga780619.gif"/>
+        first_month_images = find_images_on_page(first_month_page)
+        download_all_images_in(first_month_images)
+
+        # extract all other months
+        other_month_links = list(first_month_page.find_all('a'))
+        other_month_links = list(filter(lambda x: x.string in MONTHS, other_month_links))
+
+        for other_month_link in other_month_links:
+            other_month_page = get_soup(read_url(BASE_URL + other_month_link['href']))
+            other_month_images = find_images_on_page(other_month_page)
+            download_all_images_in(other_month_images)
